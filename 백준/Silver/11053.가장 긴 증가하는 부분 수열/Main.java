3c8a87a63ccdd00a4e390ec2c9d1be34d4613ef1
nijeysh <yejin6521@naver.com>
2025-04-08 21:59:59 +0900

    가장 긴 증가하는 부분 수열

---
+++ 백준/Silver/11053.가장 긴 증가하는 부분 수열/Main.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class Main {
+    /**
+     * 수열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열을 구하는 프로그램을 작성하시오.
+     * 예를 들어, 수열 A = {10, 20, 10, 30, 20, 50} 인 경우에 가장 긴 증가하는 부분 수열은 A = {10, 20, 10, 30, 20, 50} 이고, 길이는 4이다.
+     */
+    public static void main(String[] args) throws Exception {
+        int N = read();
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = read();
+        }
+
+        // i번째 원소를 마지막으로 하는 부분 수열의 최대 길이
+        int[] dp = new int[N];
+        dp[0] = 1;
+        for (int i = 1; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+//        System.out.println(Arrays.toString(dp));
+        Arrays.sort(dp);
+//        System.out.println(Arrays.toString(dp));
+        System.out.println(dp[N - 1]);
+    }
+
+    static int read() throws Exception {
+        int c, n, sign = 1;
+        while ((c = System.in.read()) <= 32);
+        if (c == '-') {
+            sign = -1;
+            c = System.in.read();
+        }
+        n = c & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n * sign;
+    }
+}
