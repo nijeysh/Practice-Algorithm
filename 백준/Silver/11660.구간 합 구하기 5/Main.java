d9aa66d1372d0cc1f7103f2dcfbfb4a72fdcfb38
nijeysh <yejin6521@naver.com>
2025-04-19 18:26:46 +0900

    refactor: 구간 합 구하기

--- 백준/Silver/11660.구간 합 구하기 5/Main.java
+++ 백준/Silver/11660.구간 합 구하기 5/Main.java
@@ -7,26 +7,21 @@ public class Main {
 
         int[][] arr = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
-            for (int j = 0; j < N; j++) {
-                arr[i][j + 1] += arr[i][j] + read();
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = read() + arr[i-1][j] + arr[i][j-1] - arr[i-1][j-1];
             }
         }
 
-//        System.out.println(Arrays.deepToString(arr));
+        System.out.println(Arrays.deepToString(arr));
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             int x1 = read();
             int y1 = read();
             int x2 = read();
             int y2 = read();
-            int sum = 0;
-            for (int row = x1; row <= x2; row++) {
-                sum += arr[row][y2] - arr[row][y1 - 1];
-//                System.out.print(arr[row][y2] - arr[row][y1 - 1]);
-//                System.out.print(" + ");
-            }
+            int sum = arr[x2][y2] - arr[x2][y1-1] - arr[x1-1][y2] + arr[x1-1][y1-1];
             sb.append(sum).append("\n");
-//            System.out.println();
         }
         System.out.print(sb);
     }
