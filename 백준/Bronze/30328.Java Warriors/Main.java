8773fa6d6d36d337d5604de1a5c459140b93b435
nijeysh <yejin6521@naver.com>
2025-06-09 23:43:58 +0900

    Java Warriors

---
+++ 백준/Bronze/30328.Java Warriors/Main.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long totalFee = (long) n * 4000;
+
+        System.out.println(totalFee);
+        br.close();
+    }
+}
