7040231c822c09cd8fbad674fff3d3e91bff9e67
nijeysh <yejin6521@naver.com>
2025-07-14 23:03:17 +0900

    접두사

---
+++ 백준/Silver/1141.접두사/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());    // 단어의 개수
+
+        String[] arr = new String[N];
+        for (int i = 0; i < N; i++) {
+            String word = br.readLine();
+            arr[i] = word;
+        }
+
+        Arrays.sort(arr);
+        int[] count = new int[N];
+
+        // System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < N; i++) {
+            String str = arr[i];
+            for (int j = i; j < N; j++) {
+                if (!arr[j].startsWith(str)) {
+                    count[j] = count[i]+1;
+                }
+            }
+        }
+
+        Arrays.sort(count);
+
+        System.out.println(count[N-1]+1);
+    }
+}
