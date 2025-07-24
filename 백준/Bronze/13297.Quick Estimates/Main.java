a46857bac8ad8dc927eda93d9a6d90d8e80f4101
nijeysh <yejin6521@naver.com>
2025-07-24 23:21:29 +0900

    Quick Estimates

---
+++ 백준/Bronze/13297.Quick Estimates/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            String cost = br.readLine();
+            sb.append(cost.length()).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
