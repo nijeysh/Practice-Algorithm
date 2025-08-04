1ededf987be685445441d5a28e3b1422d543cde5
nijeysh <yejin6521@naver.com>
2025-08-04 23:46:19 +0900

    카드 게임

---
+++ 백준/Bronze/5522.카드 게임/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int num = Integer.parseInt(br.readLine());
+            sum += num;
+        }
+
+        System.out.println(sum);
+    }
+}
