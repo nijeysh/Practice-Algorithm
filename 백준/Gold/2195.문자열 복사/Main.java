86f7ebf4258cffe10e17ef1a8f8611bcc5ecfe10
nijey <yejin6521@naver.com>
2025-09-05 23:57:31 +0900

    문자열 복사

---
+++ 백준/Gold/2195.문자열 복사/Main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String p = br.readLine();
+
+        int idx = 0;
+        int res = 0;
+
+        for (int i = 0; i < p.length(); i++) {
+            if (s.indexOf(p.substring(idx, i+1)) != -1) {
+                continue;
+            }
+
+            res++;
+            idx = i;
+        }
+        System.out.println(res+1);
+
+    }
+}
