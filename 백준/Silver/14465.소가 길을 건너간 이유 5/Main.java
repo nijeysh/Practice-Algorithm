fe960ce68c5ac39ca0f053e8bb3873b9a2ecf68f
nijey <yejin6521@naver.com>
2025-10-05 21:03:42 +0900

    refactor: 소가 길을 건너간 이유 5

--- 백준/Silver/14465.소가 길을 건너간 이유 5/Main.java
+++ 백준/Silver/14465.소가 길을 건너간 이유 5/Main.java
@@ -1,46 +1,30 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         int N = read(); // 횡단보도 개수
-        int K = read(); // 연속한 K개의 신호등이 존재해야함
+        int K = read(); // 연속한 K개의 신호등이 존재해야 함
         int B = read(); // 고장난 신호등 개수
 
-        int[] lights = new int[N+1];
+        int[] lights = new int[N + 1];
         for (int i = 0; i < B; i++) {
-            int brokenLightNum = read();
-            lights[brokenLightNum] = 1;
+            int broken = read();
+            lights[broken] = 1; // 고장
         }
-        // 최소 몇 개의 신호등을 수리해야 하는가
-        int currentBrokenLights = 0;
-        int min = 0;    // 최소 개수 수리
-        int left = 1;
-        int right = 0;
 
-        // 첫번째 슬라이딩
-        while (true) {
-            // K개만큼 가져야됨
-            if (right < K) {
-                right++;
-                // 수리해야할 개수
-                min += lights[right];
-                currentBrokenLights += lights[right];
-            } else if (right+1 > N) {
-                break;
-            } else {
-                // System.out.println("currentBrokenLights: " + currentBrokenLights);
-                // System.out.println("right: " + right);
-                // 다음 슬라이딩 계산
-                int prevLeft = lights[left];
+        // 초기 윈도우 (1 ~ K)
+        int brokenCount = 0;
+        for (int i = 1; i <= K; i++) {
+            brokenCount += lights[i];
+        }
 
-                left++;
-                right++;
+        int min = brokenCount;
 
-                int nextRight = lights[right];
-
-                currentBrokenLights = currentBrokenLights - prevLeft + nextRight;
-                min = Math.min(min, currentBrokenLights);
-            }
+        // 슬라이딩 윈도우 (2 ~ N-K+1)
+        for (int left = 2; left + K - 1 <= N; left++) {
+            int right = left + K - 1;
+            brokenCount = brokenCount - lights[left - 1] + lights[right];
+            min = Math.min(min, brokenCount);
         }
 
         System.out.println(min);
