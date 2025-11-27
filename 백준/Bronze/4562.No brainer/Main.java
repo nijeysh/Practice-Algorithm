0804edcc1004302fecc2e94b1a7873c4ca0b5f18
nijeysh <yejin6521@naver.com>
2025-11-27 23:53:50 +0900

    No brainer

---
+++ 백준/Bronze/4562.No brainer/Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // 테스트 케이스 수
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (x < y) {
+                System.out.println("NO BRAINS");
+            } else {
+                System.out.println("MMM BRAINS");
+            }
+        }
+    }
+}
